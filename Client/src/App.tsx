fdd65b83e3adcae44d4042b1568220daaaed7ab2
Patrik_Jørgensen <vadmand806@gmail.com>
2024-03-08 10:24:15 +0100

    App router

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 import { Map } from './Components/maptest/maptest'
-
+import { HomePage } from './Pages/HomePage/HomePage'
 function App() {
 
   return (
     <>
-      <Map></Map>
+      <HomePage></HomePage>
     </>
   )
 }
---
+++ Client/src/Components/Cards/Card.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './card.module.scss'
+interface CardProps {
+  title: string;
+  text: string;
+  icon: string;
+}
+
+export const Card: React.FC<CardProps> = ({ title, text, icon }) => {
+  return (
+    <div className={styles.card}>
+      <img src={icon} alt="" />
+      <div className={styles.textWrapper}>
+      <h3>{title}</h3>
+      <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
