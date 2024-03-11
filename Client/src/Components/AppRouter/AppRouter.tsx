a613d1dccffa2c1f53e45a10dd5e5e7c9a6ed208
Cornelliah <horianehkr@yahoo.com>
2024-03-11 09:12:55 +0000

    working on tips page

--- Client/src/Components/AppRouter/AppRouter.tsx
+++ Client/src/Components/AppRouter/AppRouter.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import { HomePage } from "../../Pages/HomePage/HomePage"
+import { TipsPage } from "../../Pages/Tips page/TipsPage"
 
 
 export const AppRouter = () => {
     return (
         <Routes>
             <Route index element={<HomePage />}></Route>
-            <Route></Route>
+            <Route path="tips" element={<TipsPage />} />
         </Routes>
         
         
