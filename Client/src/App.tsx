6df2e4624388911a723e5a1d8ccede5c4ae908ef
Patrik_Jørgensen <vadmand806@gmail.com>
2024-03-11 14:17:07 +0100

    navbar

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,20 +1,12 @@
 import './App.scss'
 // import { Map } from './Components/maptest/maptest'
-<<<<<<< HEAD
-import { HomePage } from './Pages/HomePage/HomePage'
-=======
 import { AppRouter } from './Components/AppRouter/AppRouter'
 
->>>>>>> 84bccfacbe9977be28907b8fafc2c76a4bb2e705
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <HomePage></HomePage>
-=======
     <AppRouter></AppRouter>
->>>>>>> 84bccfacbe9977be28907b8fafc2c76a4bb2e705
     </>
   )
 }
--- Client/src/Components/AppRouter/AppRouter.tsx
+++ Client/src/Components/AppRouter/AppRouter.tsx
@@ -1,28 +1,21 @@
 import { Route, Routes } from "react-router-dom"
 import { HomePage } from "../../Pages/HomePage/HomePage"
 
-<<<<<<< HEAD
-=======
 import { TipsPage } from "../../Pages/Tips page/TipsPage"
 
 import { PageNotFound } from "../../Pages/PageNotFound/PageNotFound"
 
 
->>>>>>> 84bccfacbe9977be28907b8fafc2c76a4bb2e705
 
 export const AppRouter = () => {
     return (
         <Routes>
             <Route index element={<HomePage />}></Route>
-<<<<<<< HEAD
-            <Route></Route>
-=======
 
             <Route path="tips" element={<TipsPage />} />
 
             <Route path="*" element={<PageNotFound />}></Route>
 
->>>>>>> 84bccfacbe9977be28907b8fafc2c76a4bb2e705
         </Routes>
         
         
