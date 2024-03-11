f43ee880e4498f26de2df6516d9209b1b2c4bf38
Cornelliah <horianehkr@yahoo.com>
2024-03-11 09:47:39 +0000

    working on tips page

--- Client/src/Pages/Tips page/TipsPage.tsx
+++ Client/src/Pages/Tips page/TipsPage.tsx
@@ -1,11 +1,21 @@
 
-import { HomePage } from "../HomePage/HomePage"
+import React from 'react';
+import { Card, Space } from 'antd';
 
 export const TipsPage = () => {
     return (
 
         <>
-         <h3>Welcome to you eco encyclopedia</h3>
+         <h3>Welcome to you eco encyclopedia!</h3>
+        
+         <Space direction="vertical" size={16}>
+     <Card title="DAILY TIPS"  style={{ width: 300 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+    
+  </Space>
         </>
     )
   
