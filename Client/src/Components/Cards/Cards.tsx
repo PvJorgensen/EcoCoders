eec9013852ea3285ba02abc5e70ebd4aac8170e3
Patrik_Jørgensen <vadmand806@gmail.com>
2024-03-12 10:14:32 +0100

    test

--- Client/src/Components/Cards/Cards.tsx
+++ Client/src/Components/Cards/Cards.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card } from './Card';
 
 const arrcards = [
-  { title: 'Events', text: 'There is many events happing in your city. Check them out!', icon: 'events.svg', link: '/' },
+  { title: 'Events', text: 'There is many events happing in your city. Check them out!', icon: 'events.svg', link: '/events' },
   { title: 'Challenges', text: 'You can create challenges and invite your frends to take part', icon: 'Challenges.svg', link: '/PageNotFound' },
   { title: 'Friends', text: 'Check out what challenges your friends have been completing', icon: 'Friends.svg', link: '/PageNotFound' },
 ];
