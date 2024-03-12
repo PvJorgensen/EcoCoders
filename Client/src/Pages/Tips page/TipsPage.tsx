489aef53d32539f616664226bd1de5ea5c032b2c
Cornelliah <horianehkr@yahoo.com>
2024-03-12 10:56:41 +0000

    using the tipService

--- Client/src/Pages/Tips page/TipsPage.tsx
+++ Client/src/Pages/Tips page/TipsPage.tsx
@@ -1,21 +1,42 @@
 import { Card } from 'antd';
-import { useState } from 'react';
+import {useEffect,useState } from 'react';
 import axios from 'axios';
 import Searchbar from '../../Components/Search bar/Searchbar';
 import styles from './TPage.module.scss';
+import TipsService from '../../services/tips.service';
+import { Link } from 'react-router-dom'; 
 
 interface Suggestion {
     [key: string]: any; 
 }
+interface Tip {
+    id: number;
+    name: string;
+    description: string;
+    category: string;
+}
 
 export const TipsPage = () => {
     const [result, setResult] = useState<Suggestion | null>(null);
+    const { getAllTipss } = TipsService();
+    const [tips, setTips] = useState<Tip[]>([]);
 
     const fetchData = async (value: string) => {
         const { data } = await axios.get(`https://dummyjson.com/products/search?q=${value}&limit=10`);
         return data.products;
     };
 
+    useEffect(() => {
+        async function fetchTips() {
+            try {
+                const tipsData = await getAllTipss();
+                setTips(tipsData);
+            } catch (error) {
+                console.error('Error fetching tips:', error);
+            }
+        }
+        fetchTips();
+    }, []);
     return (
         <>
             <h2 className={`${styles.h2} `}>Welcome to your eco-encyclopedia!</h2>
@@ -39,6 +60,20 @@ export const TipsPage = () => {
                     <Searchbar fetchData={fetchData} setResult={setResult} suggestionKey="category"   />
                 </div>
             </div>
+            <div>
+                {Array.isArray(tips) ? (
+                    tips.map(tip => (
+                     <Link to={`/categorizedTips/${encodeURIComponent(tip.category)}`} key={tip.id}>
+                        <Card title={tip.category} style={{ width: 353, height: 144, margin: '10px', borderRadius: '10px', border: '1px solid #A8DEE6' }} />
+                    </Link>
+
+                        
+                    ))
+                ) : (
+                    <p>No tips available</p>
+                )}
+            </div>
+         
         </>
     );
 };
