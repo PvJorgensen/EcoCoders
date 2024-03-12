25dc45d0614d0ddc0e8f5cc125153969151c41ac
solal <solald@gmail.com>
2024-03-12 08:42:30 +0000

    events page conn.to supabase

--- Client/src/Pages/EventsPage/Events.tsx
+++ Client/src/Pages/EventsPage/Events.tsx
@@ -1,43 +1,52 @@
 import { useEffect, useState } from 'react';
-import { useSupabase } from '../../Providers/SupabaseProvider/SupabaseProvider';
 import { EventCard } from '../../Components/EventCards/EventCard';
+import EventService from '../../services/event.service';
 
 export const Events = () => {
-    interface Event {  //defining the event
+    interface Event {
         id: number;
-        title: string;
-        text: string;
-        icon: string;
-        link: string;
-      }
+        name: string;
+        description: string;
+        longitude: number;
+        latitude: number;
+        date_start: Date;
+        date_end: Date;
+    }
+    const { getAllEvents } = EventService();  //fetching events
+    const [events, setEvents] = useState<Event[]>([]);
 
-  const [events, setEvents] = useState<Event[]>([]);
-  const { supabase } = useSupabase(); // getting the supabase client
-  console.log(supabase)
+    useEffect(() => {
+        async function fetchEvents() {
+            try {
+                const eventsData = await getAllEvents();
+                setEvents(eventsData);
+            } catch (error) {
+                console.error('Error fetching events:', error);
+            }
+        }
+        fetchEvents();
+    }, []);
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const { data: events, error } = await supabase
-        .from('events')
-        .select('*');
-  
-      if (error) console.error('Error loading events', error);
-      else {
-        console.log('Events fetch:', events);
-        setEvents(events);
-      }
-    };
-  
-    fetchEvents();
-  }, []);
-  
-
-  return (
-    <div>
-      {events.map((event) => (
-        <EventCard key={event.id} title={event.title} text={event.text} icon={event.icon} link={event.link} />
-      ))}
-    </div>
-  );
-};
+    return (
+        <>
+            <div>
+                {Array.isArray(events) ? (
+                    events.map(event => (
+                        <EventCard key={event.id} 
+                        id={event.id}
+                        name={event.name} 
+                        description={event.description}
+                        longitude={event.longitude}
+                        latitude={event.latitude}
+                        date_start={event.date_start}
+                        date_end={event.date_end}
+                        />
+                    ))
+                ) : (
+                    <p>No events available</p>
+                )}
+            </div>
+        </>
+    )
+}
 
