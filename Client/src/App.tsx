f35b9aa110bbd3bf8ea9755a81ef5fbe657365b7
solal <solald@gmail.com>
2024-03-12 10:31:13 +0000

    minor fixes on merging tips to develop

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-import './App.css'
-
-// import { Map } from './Components/maptest/maptest'
-
-
-=======
 import './App.scss'
 // import { Map } from './Components/maptest/maptest'
->>>>>>> 5a16d297c81a6afb7ff83cf472f0ee27806cab33
 import { AppRouter } from './Components/AppRouter/AppRouter'
 
 function App() {
@@ -15,10 +7,6 @@ function App() {
   return (
     <>
     <AppRouter></AppRouter>
-<<<<<<< HEAD
-
-=======
->>>>>>> 5a16d297c81a6afb7ff83cf472f0ee27806cab33
     </>
   )
   
