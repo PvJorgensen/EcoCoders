6da812f9102d02d798066a01208531121bc107ef
Patrik_Jørgensen <vadmand806@gmail.com>
2024-03-13 14:30:32 +0100

    data fixes on events

--- Client/src/Components/EventCards/EventCard.tsx
+++ Client/src/Components/EventCards/EventCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './events.module.scss'
 import { Link } from 'react-router-dom';
+import { dateToNumbers } from '../utils/date_utils';
 
 interface EventCardProps {
   id: number;
@@ -13,13 +14,17 @@ interface EventCardProps {
 }
 
 export const EventCard: React.FC<EventCardProps> = ({ id, name, longitude, latitude, date_start, date_end }) => {
+  const date_end_formatted = new Date(date_end)
+  const date_start_formatted = new Date(date_start)
+  
   return (
     <Link to={`/event/${id}`} className={style.card} key={id}>
       <div>img</div>
           <div className={style.mainText}>
             <h3>{name}</h3>
             <p>Button{longitude} - {latitude}</p>
-            <p>{new Date(date_start).toDateString()} - {new Date(date_end).toDateString()}</p>
+            <p>{dateToNumbers(date_start_formatted)}</p>
+            <p>{`${date_start_formatted.getHours()}:${date_start_formatted.getMinutes()} `} - {`${date_end_formatted.getHours()}:${date_end_formatted.getMinutes()} `}</p>
             <p></p>
           </div>
           <div>button</div>
