ca7a81a3561d09cb0f1601bd42a0bd7a1a409238
adanelrincon <adanperezhernandez@alumno.ieselrincon.es>
2024-03-13 21:55:44 +0000

    userProfile

--- Client/src/Pages/HomePage/HomePage.tsx
+++ Client/src/Pages/HomePage/HomePage.tsx
@@ -1,15 +1,8 @@
-//Components
 import { Cards } from "../../Components/Cards/Cards"
 import styles from "./Hpage.module.scss"
 import { Circle } from "../../Components/Circles/Circle"
 import { Navigation } from "../../Components/navBar/Navigation"
 
-import { supabase } from "../../services/clientSupabase";
-
-const handleSignOut = async () => {
-  const { error } = await supabase.auth.signOut();
-  if (error) console.log("Error signing out:", error.message);
-}
 export const HomePage = () => {
   return (
     <>
@@ -18,7 +11,6 @@ export const HomePage = () => {
         <h3>Welcome "Username"</h3>
         <p>I hope you are eager to start improving the planet and challenge your friends to do different challenges</p>
       </section>
-      <button onClick={handleSignOut}>SignOut</button>
       <Cards />
       <Navigation />
     </>
