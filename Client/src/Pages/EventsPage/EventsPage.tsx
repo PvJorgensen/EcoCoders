d046fac3b3b883b71fb7cab4d92edf7caa6272ae
solal <solald@gmail.com>
2024-03-13 13:00:15 +0000

    added buttons, styling, fixed issue with layout and showing events. Added MapView Button in All Events.

--- Client/src/Pages/EventsPage/EventsPage.tsx
+++ Client/src/Pages/EventsPage/EventsPage.tsx
@@ -1,17 +1,26 @@
 
+// This Page renders all the events
+import { useNavigate } from "react-router-dom";
 import { Navigation } from "../../Components/navBar/Navigation";
 import { Events } from './Events'
+import { Button } from "antd";
+import { EnvironmentFilled } from "@ant-design/icons";
 
 
 
-  export const EventsPage: React.FC = () => {
+export const EventsPage: React.FC = () => {
+  const navigate= useNavigate();
+  const openMapPage = () => {
+    navigate( "/map");
+  }
 
-    return (
-      <div>
-        <h1>EVENTS</h1>
-        <Events />
-        <Navigation />
-      </div>
-    );
-  };
+  return (
+    <div>
+      <h1>EVENTS</h1>
+      <Button icon={<EnvironmentFilled />}type="default" shape="circle" size="large"onClick={openMapPage} />
+      <Events />
+      <Navigation />
+    </div>
+  );
+};
   
