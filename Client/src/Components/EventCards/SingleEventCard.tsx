7942aaabd382c2199ec8520d8dee3f38890274d2
Patrik_Jørgensen <vadmand806@gmail.com>
2024-03-14 11:35:07 +0100

    single event changes

--- Client/src/Components/EventCards/SingleEventCard.tsx
+++ Client/src/Components/EventCards/SingleEventCard.tsx
@@ -1,10 +1,12 @@
 //page for single event card
+import { EnvironmentOutlined } from '@ant-design/icons';
 
 import React from 'react';
 import style from './singleEvent.module.scss'
 import { Navigation } from '../navBar/Navigation'
 
 import { dateToNumbers } from '../utils/date_utils';
+import { months } from '../utils/date_utils';
 
 
 interface EventCardProps {
@@ -18,7 +20,7 @@ interface EventCardProps {
   img: string;
 }
 
-export const SingleEventCard: React.FC<EventCardProps> = ({ id, name, description, longitude, latitude, date_start, date_end, img }) => {
+export const SingleEventCard: React.FC<EventCardProps> = ({ id, name, description, date_start, date_end, img }) => {
   const date_end_formatted = new Date(date_end)
   const date_start_formatted = new Date(date_start)
   return (
@@ -28,20 +30,23 @@ export const SingleEventCard: React.FC<EventCardProps> = ({ id, name, descriptio
       <img src={img} alt="event_img" />
         <div className={style.mainText}>
           <h3>{name}</h3>
-          <p>{description}</p>
-        </div>
-        <div className={style.location}>
-          <p>{longitude}{latitude}</p>
+          <a href="/map"><EnvironmentOutlined /></a>
         </div>
         <div className={style.dates}>
-           {/* <p>{new Date(date_start).toDateString()} - {new Date(date_end).toDateString()}</p> */}
-          <p>{dateToNumbers(date_start_formatted)}</p>
-          <p>{`${date_start_formatted.getHours()}:${date_start_formatted.getMinutes()} `} - {`${date_end_formatted.getHours()}:${date_end_formatted.getMinutes()} `}</p>          <p></p>
+          <p className={style.datatext}>{date_start_formatted.getDate()} {months[date_start_formatted.getMonth()]}</p>
+          <p className={style.datatext}>{`${date_start_formatted.getHours()}:${date_start_formatted.getMinutes()} `} | {`${date_end_formatted.getHours()}:${date_end_formatted.getMinutes()} `}</p>          <p></p>
+          <div className={style.location}>
+          <p>{description}</p>
+          
+        </div>
         </div>
       </div>
+          <div className={style.joinbtn}>
+            <button>JOIN</button>
+          </div>
     </div>
     <Navigation></Navigation>
-    
+
     </div>
   );
 };
